e4b7e89dba609e91ebc0e6b0c86595fc36ee839b
Daniel Shi <danielshi96@gmail.com>
2025-07-16 21:27:09 -0700

    Base

---
+++ src/app/collections/project.ts
@@ -0,0 +1,106 @@
+import { CollectionConfig, FieldHook } from 'payload'
+
+const format = (val: string): string =>
+  val
+    .replace(/ /g, '-')
+    .replace(/[^\w-/]+/g, '')
+    .toLowerCase()
+
+const formatSlug =
+  (fallback: string): FieldHook =>
+  ({ value, originalDoc, data }) => {
+    if (typeof value === 'string') {
+      return format(value)
+    }
+    const fallbackData = data?.[fallback] || originalDoc?.[fallback]
+
+    if (fallbackData && typeof fallbackData === 'string') {
+      return format(fallbackData)
+    }
+
+    return value
+  }
+
+export const ProjectCollections: CollectionConfig = {
+  slug: 'movies',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {
+    create: () => true,
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'url',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'votes',
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'poster',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'overview',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'tagline',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'genres',
+      type: 'array',
+      fields: [
+        {
+          name: 'name',
+          type: 'text',
+        },
+      ],
+      required: true,
+    },
+    {
+      name: 'gallery',
+      type: 'array',
+      label: 'Image Gallery',
+      fields: [
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+          required: true,
+        },
+        {
+          name: 'caption',
+          type: 'text',
+          required: false,
+        },
+      ],
+    },
+    {
+      name: 'slug',
+      label: 'Slug',
+      type: 'text',
+      admin: {
+        position: 'sidebar',
+      },
+      hooks: {
+        beforeValidate: [formatSlug('name')],
+      },
+    },
+  ],
+}
